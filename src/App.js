dc2ec84a618cfebcaac5d8344877e52a25e869d4
Vardan Khachatryan <134618862+Vardan-Khachatryan@users.noreply.github.com>
2023-08-23 20:17:26 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from "./pages/About/About";
+import { Home } from "./pages/Home/Home";
+import { Contact } from "./pages/Contact/Contact";
+import { Projects } from "./pages/Projects/Projects";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="projects" element={<Projects />} />
+        {/* <Route path="*" element={<Error />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
